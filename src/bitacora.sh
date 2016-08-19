d406b2a43e5cbc22d86a583641066249b7b67d69
Fernando Pineda <wfpinedar@gmail.com>
2016-08-19 01:49:45 -0500

    Se ajusta el script con rationales, links y comments sobre los pasos a seguir

--- src/bitacora.sh
+++ src/bitacora.sh
@@ -1,35 +1,71 @@
 #!/bin/bash
+echo Bienvenido al instalador de cubo de datos del Fondo Patrimonio Natural. author: @wfpinedar
+echo Instalando utilidades necesarias para el script...
+# rationale: figlet: muestra letreros vistosos en terminal
+sudo yum install -y \
+figlet \
+sudo
+
 # Install Deps
-sudo yum install -y epel-release
-sudo yum localinstall -y http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-1.noarch.rpm
+figlet -f big Instalando Dependencias
+# Se actualiza como recomienda la documentación de Red Hat
+# link: https://access.redhat.com/articles/11258
 sudo yum update -y
+sudo yum install -y epel-release
+# Repositorio de postgresql oficial
+# link: https://www.postgresql.org/download/linux/redhat/
+sudo yum localinstall -y http://yum.postgresql.org/9.3/redhat/rhel-7-x86_64/pgdg-centos93-9.3-1.noarch.rpm
+# rationale: postgresql93-server postgresql93 postgresql93-contrib postgresql93-libs: Instalación de PostgreSQL
+# rationale: PyQt4 PyQt4-devel PyQt4-webkit: 
+# rationale: gdal gdal-devel gdal-libs gdal-python:
+# rationale: numpy numpy-f2py:
+# rationale: postgis2_93 postgis2_93-client postgis2_93-debuginfo postgis2_93-devel postgis2_93-docs postgis2_93-utils:
+# rationale: python-psycopg2:
+# rationale: pytz:
+# rationale: scipy:
+# rationale: pgadmin3_93: Administrador gráfico de bases de datos ¿No necesario?
+# rationale: environment-modules:
+# rationale: wget: Descargador de recursos de red
+# rationale: git: Gestor de versionamiento de código
+# rationale: gcc gcc-c++: Compiladores de código C, C++ y otros
+# rationale: unzip: Descompresor de archivos ZIP
 sudo yum install -y postgresql93-server postgresql93 postgresql93-contrib postgresql93-libs PyQt4 PyQt4-devel PyQt4-webkit gdal gdal-devel gdal-libs gdal-python numpy numpy-f2py postgis2_93 postgis2_93-client postgis2_93-debuginfo postgis2_93-devel postgis2_93-docs postgis2_93-utils python-psycopg2 pytz scipy pgadmin3_93 environment-modules wget git gcc gcc-c++ unzip
+# rationale: pip: Python Packaget Index: Gestor recomendado para instalar paquetes
+# link: https://packaging.python.org/current/
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
+# Instalación de paquetes de python
 sudo pip install numexpr
 sudo pip install ephem
-git clone https://github.com/GeoscienceAustralia/EO_tools.git --depth=10
+
 # Install EOtools
+# Descarga del repositorio de EOtools
+git clone https://github.com/GeoscienceAustralia/EO_tools.git -b master --single-branch --depth=10
+figlet -f big Instalando EOtools
 cd EO_tools/
+# Instalan dependencias
 sudo python setup.py install
 cd ..
+
 # Download and Install Data Cube
+figlet -f big Instalando Data Cube
 # Only get the develop branch
 git clone -b develop --single-branch https://github.com/GeoscienceAustralia/agdc.git
 # Change agdc_default.config
-vim agdc/agdc/agdc_default.config
 cp agdc/agdc/agdc-example.conf agdc/agdc/agdc_default.config
 sed -i.bak 's/host\ \=\ 130\.56\.244\.224/host\ \=\ 0\.0\.0\.0/g' agdc/agdc/agdc_default.config
 sed -i 's/port = 6432/port = 5432/g' agdc/agdc/agdc_default.config
 sed -i 's/dbname = datacube/dbname = postgres/g' agdc/agdc/agdc_default.config
 # Install Data Cube
 cd agdc
+echo Instalando dependencias AGDC
 sudo python setup.py install --force
 # Necessary Modifications
 # Update config file for API
 sed -i.bak 's/HOST = "host"/HOST = "0.0.0.0"/g' api/source/main/python/datacube/config.py
 sed -i 's/PORT = "port"/PORT = "3128"/g' api/source/main/python/datacube/config.py
 sed -i 's/DATABASE = "database"/DATABASE = "postgres"/g' api/source/main/python/datacube/config.py
+cd ..
 sudo /usr/pgsql-9.3/bin/postgresql93-setup initdb
 sudo systemctl enable postgresql-9.3.service
 sudo systemctl start postgresql-9.3.service
@@ -43,11 +79,29 @@ create group cube_admin_group with superuser;
 \q
 EOF
 sudo chown postgres:postgres /tmp/create_databases_roles.sql
-sudo su postgres -c "
-psql -f /tmp/create_databases_roles.sql
-"
+sudo -u postgres psql -f /tmp/create_databases_roles.sql
 # Change permissions
-#vim /var/lib/pgsql/9.3/data/pg_hba.conf
 #Towards the bottom, the second local host should be trust
 #Hacia la parte inferior, el segundo anfitrión local debe ser de confianza.
-#exit
+sed -i.bak '/^local/ s/peer/trust/' /var/lib/pgsql/9.3/data/pg_hba.conf
+mkdir scripts_sql
+cd $_
+wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v1__schema.sql
+wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v2__base_data.sql
+wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v3__additional_constraints.sql
+wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v4__modis.sql
+wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v5__wofs.sql
+wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v6__index.sql
+for i in $(ls)
+do
+  echo Ejecutando: $i
+  sudo -u postgres psql < $i
+done
+cd ..
+
+#Install API
+#sudo python setup.py install –force
+# Create directory to store tiles
+cd agdc #¿?
+sudo mkdir /g/
+chmod 777 /g/
