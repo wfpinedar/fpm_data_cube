1ca098c8e9ac165841476a902e37b0a58d0efe64
Fernando Pineda <wfpinedar@gmail.com>
2016-08-19 02:40:22 -0500

    Haciendo el script idempotente idempotent

--- src/bitacora.sh
+++ src/bitacora.sh
@@ -41,16 +41,25 @@ sudo pip install ephem
 
 # Install EOtools
 # Descarga del repositorio de EOtools
-git clone https://github.com/GeoscienceAustralia/EO_tools.git -b master --single-branch --depth=10
+if [ -d EO_tools ]
+then
+  echo 'EO_tools ya está instalado. Nada que hacer.'
+else 
+git clone https://github.com/GeoscienceAustralia/EO_tools.git -b stable --single-branch --depth=10
 figlet -f big Instalando EOtools
 cd EO_tools/
 # Instalan dependencias
 sudo python setup.py install
 cd ..
+fi #EO_tools
 
 # Download and Install Data Cube
 figlet -f big Instalando Data Cube
 # Only get the develop branch
+if [ -d agdc ]
+then
+  echo 'agdc ya está instalado. Nada que hacer.'
+else
 git clone -b develop --single-branch https://github.com/GeoscienceAustralia/agdc.git
 # Change agdc_default.config
 cp agdc/agdc/agdc-example.conf agdc/agdc/agdc_default.config
@@ -67,10 +76,22 @@ sed -i.bak 's/HOST = "host"/HOST = "0.0.0.0"/g' api/source/main/python/datacube/
 sed -i 's/PORT = "port"/PORT = "3128"/g' api/source/main/python/datacube/config.py
 sed -i 's/DATABASE = "database"/DATABASE = "postgres"/g' api/source/main/python/datacube/config.py
 cd ..
+fi #agdc
+
+# Realizando configuración POSTGRESQL
+if [ -f /var/lib/pgsql/9.3/data/pg_hba.conf ]
+then
+  echo 'PostgreSQL ya está configurado. Nada que hacer.'
+else
 sudo /usr/pgsql-9.3/bin/postgresql93-setup initdb
 sudo systemctl enable postgresql-9.3.service
 sudo systemctl start postgresql-9.3.service
+fi #/var/lib/pgsql/9.3/data/pg_hba.conf
 # Create database roles
+if [ -f /tmp/create_databases_roles.sql ]
+then
+  echo '/tmp/create_databases_roles.sql existe y está ejecutado. Nada que hacer.'
+else
 tee /tmp/create_databases_roles.sql << EOF
 create user cube_admin with superuser;
 create user cube_user with superuser;
@@ -81,10 +102,22 @@ create group cube_admin_group with superuser;
 EOF
 sudo chown postgres:postgres /tmp/create_databases_roles.sql
 sudo -u postgres psql -f /tmp/create_databases_roles.sql
+fi #/tmp/create_databases_roles.sql
+
 # Change permissions
 #Towards the bottom, the second local host should be trust
 #Hacia la parte inferior, el segundo anfitrión local debe ser de confianza.
+if [ -f /var/lib/pgsql/9.3/data/pg_hba.conf.bak ]
+then
+  echo '/var/lib/pgsql/9.3/data/pg_hba.conf.bak existe. Nada que hacer.'
+else
 sed -i.bak '/^local/ s/peer/trust/' /var/lib/pgsql/9.3/data/pg_hba.conf
+fi #/var/lib/pgsql/9.3/data/pg_hba.conf.bak
+
+if [ -d scripts_sql ]
+then
+  echo 'scripts_sql existe. Nada que hacer.'
+else
 mkdir scripts_sql
 cd $_
 wget https://raw.githubusercontent.com/wfpinedar/fpm_data_cube/master/src/scripts_sql/v1__schema.sql
@@ -99,10 +132,14 @@ do
   sudo -u postgres psql < $i
 done
 cd ..
-
+fi #scripts_sql
 #Install API
 #sudo python setup.py install –force
 # Create directory to store tiles
-cd agdc #¿?
+if [ -d /g ]
+then
+  echo 'scripts_sql existe. Nada que hacer.'
+else
 sudo mkdir /g/
 chmod 777 /g/
+fi # /g
